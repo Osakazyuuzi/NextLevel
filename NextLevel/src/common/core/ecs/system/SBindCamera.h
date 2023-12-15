945dcba6ba0be22a379f6fb803d1f08254124304
Osakazyuuzi <shiraisouma1212@icloud.com>
2023-12-15 12:11:04 +0900

    名前空間を修正

--- NextLevel/src/common/core/ecs/system/SBindCamera.h
+++ NextLevel/src/common/core/ecs/system/SBindCamera.h
@@ -15,58 +15,61 @@ namespace NextLevel
 			class CTransform;
 		}
 
-		class SBindCamera : public SystemBase {
-		public:
-			/**
-			* @brief コンストラクタ。
-			* @param _pWorld ワールドポインタ。
-			*/
-			SBindCamera(World* _pWorld)
-				: SystemBase(_pWorld)
-			{}
+		namespace system
+		{
+			class SBindCamera : public SystemBase {
+			public:
+				/**
+				* @brief コンストラクタ。
+				* @param _pWorld ワールドポインタ。
+				*/
+				SBindCamera(World* _pWorld)
+					: SystemBase(_pWorld)
+				{}
 
-			/**
-			* @brief 初期化処理をします。
-			*/
-			void Init() override;
+				/**
+				* @brief 初期化処理をします。
+				*/
+				void Init() override;
 
-			/**
-			* @brief 描画処理をします。
-			*/
-			void Draw() override;
+				/**
+				* @brief 描画処理をします。
+				*/
+				void Draw() override;
 
-		private:
-			/**
-			* @brief アーキタイプに必要なコンポーネントを設定します。
-			*/
-			void ArchetypeSetting();
+			private:
+				/**
+				* @brief アーキタイプに必要なコンポーネントを設定します。
+				*/
+				void ArchetypeSetting();
 
-			/**
-			* @brief Cameraからビュー行列を算出します。
-			* @param _transform 形状データ。
-			* @return DirectX::XMMATRIX ビュー行列。
-			*/
-			DirectX::XMMATRIX CalcViewMatrix(component::CTransform _transform);
+				/**
+				* @brief Cameraからビュー行列を算出します。
+				* @param _transform 形状データ。
+				* @return DirectX::XMMATRIX ビュー行列。
+				*/
+				DirectX::XMMATRIX CalcViewMatrix(component::CTransform _transform);
 
-			/**
-			* @brief Cameraからプロジェクション行列を算出します。
-			* @param _camera カメラデータ。
-			* @return DirectX::XMMATRIX プロジェクション行列。
-			*/
-			DirectX::XMMATRIX CalcProjectionMatrix(component::CCamera _camera);
-		
-			/**
-			* @brief ビュー行列を定数バッファに書き込みます。
-			* @param _viewMat ビュー行列。
-			*/
-			void CBufWriteProjectionMatrix(DirectX::XMMATRIX _projMat);
+				/**
+				* @brief Cameraからプロジェクション行列を算出します。
+				* @param _camera カメラデータ。
+				* @return DirectX::XMMATRIX プロジェクション行列。
+				*/
+				DirectX::XMMATRIX CalcProjectionMatrix(component::CCamera _camera);
 
-			/**
-			* @brief プロジェクション行列を定数バッファに書き込みます。
-			* @param _projMat プロジェクション行列。
-			*/
-			void CBufWriteViewMatrix(DirectX::XMMATRIX _viewMat);
-		};
+				/**
+				* @brief ビュー行列を定数バッファに書き込みます。
+				* @param _viewMat ビュー行列。
+				*/
+				void CBufWriteProjectionMatrix(DirectX::XMMATRIX _projMat);
+
+				/**
+				* @brief プロジェクション行列を定数バッファに書き込みます。
+				* @param _projMat プロジェクション行列。
+				*/
+				void CBufWriteViewMatrix(DirectX::XMMATRIX _viewMat);
+			};
+		}
 	}
 }
 
